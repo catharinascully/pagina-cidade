140e1ccb9acbf3f3cc3e900fddc2ba44b0120f43
Catharina Scully <159261537+catharinascully@users.noreply.github.com>
2024-07-31 09:21:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let lamp= document.getElementById("imagem")
+let legendas = document.getElementById("legendas")
+function imag1(){
+    lamp.src= 'imagem1.jpg'
+    legendas.innerHTML="Cidade de Atenas"
+}
+
+function imag2(){
+    lamp.src= 'imagem2.jpg'
+    legendas.innerHTML="Acrópole de Atenas"
+}
+
+function imag3(){
+    lamp.src= 'imagem3.jpg'
+    legendas.innerHTML="Museu Arqueológico de Atenas"
+}
+
+function imag4(){
+    lamp.src= 'imagem4.jpg'
+    legendas.innerHTML= "Partenon"
+}
